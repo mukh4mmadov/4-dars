dd55ae183d59fc137f9dd0fe783da544a82d0b43
mukh4mmadov <omuhammadov467@gmail.com>
2024-04-25 13:46:41 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,140 @@
+//  for 
+
+// 1-masala
+
+let n = +prompt("n son kiriting");
+let k = +prompt("k son kiriting");
+for (let i = 1; i <= n; i++) {
+  console.log(k);
+}
+
+// 2-masala
+
+// let a = +prompt("a son kiriting");
+// let b = +prompt("b son kiriting");
+// let sum = 0;
+// if (a < b) {
+//   for (let i = a; i <= b; i++) {
+//     console.log(i);
+//     sum++;
+//   }
+//   console.log(`${sum} ta butun son bor`);
+// } else {
+//   console.log("a b dan kichik bolishi kerak");
+// }
+
+// 3-masala
+
+// let a = +prompt("a son kiriting");
+// let b = +prompt("b son kiriting");
+// let sum = 0;
+// if (a < b) {
+//   for (let i = b - 1; i > a; i--) {
+//     console.log(i);
+//     sum++;
+//   }
+//   console.log(`${sum} ta butun son bor`);
+// } else {
+//   console.log("a b dan kichik bolishi kerak");
+// }
+
+// 4-masala
+
+// let price = +prompt("Narx kiriting");
+// let total;
+
+// for (let i = 1; i <= 10; i++) {
+//   total = i * price;
+//   console.log(`${i}kg = ${total}sum`);
+// }
+
+// 5-masala
+
+// let price = +prompt("Narx kiriting");
+// let total;
+
+// for (let i = 0.1; i <= 1; i += 0.1) {
+//   total = i.toFixed(1) * price;
+//   console.log(`${i.toFixed(1)}kg = ${total}sum`);
+// }
+
+// 6-masala
+
+// let price = +prompt("Narx kiriting");
+// let total;
+
+// for (let i = 1; i <= 2.1; i += 0.2) {
+//   total = i.toFixed(1) * price;
+//   console.log(`${i.toFixed(1)}kg = ${total.toFixed()}sum`);
+// }
+
+// 7-masala
+
+// let a = +prompt("a son kiriting");
+// let b = +prompt("b son kiriting");
+// let sum = 0;
+// if (a < b) {
+//   for (let i = a + 1; i < b; i++) {
+//     sum += i;
+//   }
+//   console.log(` yig'indi:${sum}`);
+// } else {
+//   console.log("a b dan kichik bolishi kerak");
+// }
+
+// 8-masala
+
+// let a = +prompt("a son kiriting");
+// let b = +prompt("b son kiriting");
+// let sum = 1;
+// if (a < b) {
+//   for (let i = a + 1; i < b; i++) {
+//     sum *= i;
+//   }
+//   console.log(` ko'paytma:${sum}`);
+// } else {
+//   console.log("a b dan kichik bolishi kerak");
+// }
+
+// 9-masla
+// let a = +prompt("a son kiriting");
+// let b = +prompt("b son kiriting");
+// let sum = 0;
+// if (a < b) {
+//   for (i = a + 1; i < b; i++) {
+//     sum += i ** 2;
+//   }
+//   console.log(`butun sonlar kavadratining yig'indisi:${sum}`);
+// } else {
+//   console.log("a b dan kichik bolishi kerak");
+// }
+
+// 10-masala
+
+// let n = +prompt(" son kiriting");
+// let sum = 0;
+
+// for (i = 1; i <= n; i++) {
+//   sum += 1 / i;
+// }
+// console.log(`yig'indi: ${sum}`);
+
+// 11-masala
+
+// let n = +prompt(" son kiriting");
+// let sum = 0;
+// for (i = n; i <= n * 2; i++) {
+//   sum += i * i;
+//   console.log(i * i);
+// }
+// console.log(`yig'indi: ${sum}`);
+
+// 12-masala
+
+// let n = +prompt(" son kiriting");
+// let sum = 1;
+
+// for (i = 1; i <= n; i++) {
+//   sum *= i / 10 + 1;
+// }
+// console.log(`ko'paytma: ${sum.toFixed(3)}`);
